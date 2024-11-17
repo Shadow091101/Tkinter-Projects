8dfc6a74bb9b2bd9eb67ce58c16dec93dc6cdcd5
Shadow091101 <154974550+Shadow091101@users.noreply.github.com>
2024-11-17 10:55:32 +0530

    Update main.py

--- Digital Clock/main.py
+++ Digital Clock/main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import time
 
-
 #Refer:https://docs.python.org/3/library/time.html 
 root=Tk()
 root.geometry('500x500')
@@ -16,8 +15,6 @@ def clock():
     # ms=time.strftime('')
     my_label.config(text=hour+":"+minute+":"+second)
     my_label.after(1000,clock)
-    
-    
 
 # def update():
 #     my_label.config(text='How are you')
@@ -29,19 +26,12 @@ def laps():
     my_lap=Label(root,text=f'Lap {lap_count} :'+hour+":"+minute+":"+second)
     my_lap.pack(pady=2)
     lap_count+=1
-    
-
 
 my_label=Label(root,text='',font=("Helvetica",48),fg='green',bg="black")
 my_label.pack(pady=20)
 
 my_lap_button=Button(root,text='Lap',command=laps)
 my_lap_button.pack(pady=20)
-
-
-
-
-
 
 clock()
 
